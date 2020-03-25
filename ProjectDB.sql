814130f9f425c32ac535a20c5b69db661cabf9ff
Vasil <vacik2000@ukr.net>
2020-03-25 16:07:24 +0200

    Update ProjectDB.sql

--- ProjectDB.sql
+++ ProjectDB.sql
@@ -1,19 +1,35 @@
--- DROP DATABASE ProjectBD; 
+drop database projectbd;
+Use projectbd;
 
-CREATE DATABASE ProjectBD;
+select * from user;
 
-USE ProjectBD;
+drop table product;
+drop table user;
 
 CREATE TABLE user(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	password varchar(30) not null,
+	password varchar(30),
     email varchar(30) not null unique,
-    first_name varchar(30) not null,
-    second_name varchar(30) not null
+    first_name varchar(30),
+    second_name varchar(30) default "secondNmae!!!"
 );
 
 insert into user (first_name, second_name, email, password)
 value("lol", "fenol", "11@11", "1111");
 
+create table product (
+	id int primary key auto_increment,
+	product_name varchar (40) not null unique,
+    product_description varchar (200) default "No description",
+    price double (7, 2) not null
+);
+
+select * from product;
+
 select * from user;
 
+INSERT INTO product (product_name, product_description, price)
+VALUES ("?", "?", "?");
+
+INSERT INTO product (product_name, price)
+VALUES ("?", "?");
